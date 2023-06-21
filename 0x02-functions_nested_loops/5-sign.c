80d4cd1fe692008317fd7dd7007856796f53f12e
KiraiX77 <kiraix77@gmail.com>
2023-06-21 06:34:33 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- 
-print_sign - prints the sign of a number
-@n: n is an integer
+* 
+*print_sign - prints the sign of a number
+*@n: n is an integer
 *
-Return: 1 (if positive), 0 (if zero), -1 (if negative)
+*Return: 1 (if positive), 0 (if zero), -1 (if negative)
 */
 
 int print_sign(int n)
-{	
+{
 	if (n > 0)
 	{
 	_putchar('+');
