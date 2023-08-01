cd345468785e8bb2914bf8dbb8113970eb262858
Kiraix77 <kiraix77@gmail.com>
2023-08-01 07:21:30 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,33 +9,38 @@
 * Return: This is the adress of the new node
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *node, *new_mode = malloc(sizeof(listint_t))
-	unsigned int i = 0;
+ unsigned int i;
+ listint_t *new;
+ listint_t *temp = *head;
 
-	if (!head || !new_node)
-		return (NULL);
+ new = malloc(sizeof(listint_t));
+ if (!new || !head)
+ return (NULL);
 
-	new_node->n = n;
-	new_node->next = NULL;
-	if (!idx)
-	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
-	}
-	node = *head;
-	while (node)
-	{
-		if (i == idx - 1)
-		{
-			new_node->next = node->next;
-			node->next = new_node;
-			return (new_node);
-		}
-		i++;
-		node = node->next;
-	}
-	free(new_node);
-return (NULL);
+ new->n = n;
+ new->next = NULL;
+
+ if (idx == 0)
+ {
+ new->next = *head;
+ *head = new;
+ return (new);
+ }
+
+ for (i = 0; temp && i < idx; i++)
+ {
+ if (i == idx - 1)
+ {
+ new->next = temp->next;
+ temp->next = new;
+ return (new);
+ }
+ else
+ temp = temp->next;
+ }
+
+ return (NULL);
 }
+
