179f8f6b578417b0edaa441ec94b130025c2933f
Kiraix77 <kiraix77@gmail.com>
2023-08-01 05:34:40 -0700

    Fifth task

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * free_listint -it does free a linked list
+ * free_listint -It does free a linked list
  * @head: listint_t it's the list to be freed
  */
 void free_listint(listint_t *head)
